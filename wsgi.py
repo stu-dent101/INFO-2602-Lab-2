80aa27701d2f74f98e397db78c4cf8464dee8a4c
stu-dent101 <katoya.ottley@my.uwi.edu>
2025-01-27 20:09:18 -0400

    Initilized the application
    I got it to initialize in VS code after I commented out this line

--- wsgi.py
+++ wsgi.py
@@ -7,7 +7,7 @@ from sqlalchemy.exc import IntegrityError
 @app.cli.command("init", help="Creates and initializes the database")
 def initialize():
   db.drop_all()
-  db.init_app(app)
+  #db.init_app(app)
   db.create_all()
   bob = User('bob', 'bob@mail.com', 'bobpass')
   print(bob)
