d049b263b45acd70ef18f3b5282e32d56b76f7ae
stu-dent101 <katoya.ottley@my.uwi.edu>
2025-01-28 16:37:15 -0400

    Lab 2 Completed
    The application works

--- models.py
+++ models.py
@@ -8,10 +8,16 @@ db = SQLAlchemy(app)
 
 
 class User(db.Model):
+  __tablename__ ='user'
   id = db.Column(db.Integer, primary_key=True)
   username = db.Column(db.String(80), unique=True, nullable=False)
   email = db.Column(db.String(120), unique=True, nullable=False)
   password = db.Column(db.String(120), nullable=False)
+  #creates a relationship field to get the user's todos
+  todos = db.relationship('Todo', backref='user', lazy=True, cascade="all, delete-orphan")
+  #categories = db.relationship('Category', backref='user', lazy= 'joined') #to link Category and Todo tables together
+  categories = db.relationship('Category', lazy='joined', back_populates='user')
+
 
   def __init__(self, username, email, password):
     self.username = username
@@ -24,3 +30,85 @@ class User(db.Model):
 
   def __repr__(self):
     return f'<User {self.id} {self.username} - {self.email}>'
+  
+  
+  #"Research"
+  def add_todo_category(self, todo_id, category_name):
+        # Find the Todo by ID
+        todo = next((todo for todo in self.todos if todo.id == todo_id), None)
+        if not todo:
+            return False  # No Todo with this ID found for the user
+
+        # Check if the category already exists for this user
+        category = Category.query.filter_by(user_id=self.id, text=category_name).first()
+        if not category:
+            # Create a new category
+            category = Category(user_id=self.id, text=category_name)
+            db.session.add(category)
+            db.session.commit()
+
+        # Check if the category is already associated with the todo
+        if category not in todo.categories:
+            todo.categories.append(category)
+            db.session.commit()
+
+        return True
+  
+#####################################################################################
+
+class Todo(db.Model):
+  __tablename__ ='todo'
+  id = db.Column(db.Integer, primary_key=True)
+  user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False) #set userid as a foreign key to user.id # 1 to Many relationship from User to Todo.
+  text = db.Column(db.String(255), nullable=False)
+  task_completed = db.Column(db.Boolean, default=False)
+  #categories = db.relationship('Category', secondary='todo_category', back_populates='todos')
+
+  def toggle(self): # changed the status of task_completed from false to true and updates the database
+    self.task_completed = not self.task_completed
+    db.session.add(self)
+    db.session.commit()
+
+  def __init__(self, text):
+      self.text = text
+
+  '''
+  def __repr__(self):
+    return f'<Todo: {self.id} | {self.user.username} | {self.text} | { "Task Completed" if self.task_completed else "Task Not Completed" }>'
+  #Note: the id is for the list of todos, not the user
+  '''
+
+  def __repr__(self):
+    category_names = ', '.join([category.text for category in self.categories])
+    return f'<Todo: {self.id} | {self.user.username} | {self.text} | { "completed" if self.task_completed else "not completed" } | categories [{category_names}]>' 
+  
+#####################################################################################
+
+class TodoCategory(db.Model):
+  __tablename__ ='todo_category'
+  id = db.Column(db.Integer, primary_key=True)
+  todo_id = db.Column(db.Integer, db.ForeignKey('todo.id'), nullable=False)
+  category_id = db.Column(db.Integer, db.ForeignKey('category.id'), nullable=False)
+  last_modified = db.Column(db.DateTime, default=func.now(), onupdate=func.now())
+
+  def __repr__(self):
+    return f'<TodoCategory last modified {self.last_modified.strftime("%Y/%m/%d, %H:%M:%S")}>'
+  
+#####################################################################################
+
+class Category(db.Model):
+  __tablename__ ='category'
+  id = db.Column(db.Integer, primary_key=True)
+  user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+  text = db.Column(db.String(255), nullable=False)
+  #user = db.relationship('User', backref=db.backref('categories', lazy='joined'))
+  user = db.relationship('User', back_populates='categories')
+  todos = db.relationship('Todo', secondary='todo_category', backref=db.backref('categories', lazy=True))
+
+  def __init__(self, user_id, text):
+    self.user_id = user_id
+    self.text = text
+  
+  def __repr__(self):
+    return f'<Category user:{self.user.username} - {self.text}>'
+  
--- wsgi.py
+++ wsgi.py
@@ -1,5 +1,5 @@
 import click, sys
-from models import db, User
+from models import db, User, Todo, Category
 from app import app
 from sqlalchemy.exc import IntegrityError
 
@@ -9,16 +9,28 @@ def initialize():
   db.drop_all()
   #db.init_app(app)
   db.create_all()
+  
   bob = User('bob', 'bob@mail.com', 'bobpass')
   jane = User('jane', 'jane@mail.com', 'janepass')
   john = User('john', 'john@mail.com', 'johnpass')
+  
+  #add one task at a time
+  bob.todos.append(Todo('wash car'))
+  jane.todos.append(Todo('walk the dog'))
+  jane.todos.append(Todo('water the plants'))
+  john.todos.append(Todo('clean the house'))
+  john.todos.append(Todo('cook for the family'))
+
   db.session.add(bob)
   db.session.add(jane)
   db.session.add(john)
+
   db.session.commit()
+
   print(bob)
   print(jane)
   print(john)
+
   print('database intialized')
 
 #flask get-user
@@ -69,3 +81,83 @@ def create_user(username, email, password):
     print("Username or email already taken!") #give the user a useful message
   else:
     print(newuser) # print the newly created user
+
+#flask delete-user
+@app.cli.command('delete-user')
+@click.argument('username', default='bob')
+def delete_user(username):
+  registered_user = User.query.filter_by(username=username).first()
+  if not registered_user:
+      print(f'{username} not found!')
+      return
+  db.session.delete(registered_user)
+  db.session.commit()
+  print(f'{username} deleted')
+
+#flask get-todos
+@app.cli.command('get-todos')
+@click.argument('username', default='bob')
+def get_user_todos(username):
+  registered_user = User.query.filter_by(username = username).first()
+  if not registered_user:
+      print(f'{username} not found!')
+      return
+  print(registered_user.todos)
+
+#flask add-todo e.g. flask add-todo jane "wash the clothes"
+@app.cli.command('add-todo')
+@click.argument('username', default='bob')
+@click.argument('text', default='wash car')
+def add_task(username, text):
+  registered_user = User.query.filter_by(username=username).first()
+  if not registered_user:
+      print(f'{username} not found!')
+      return
+  new_todo = Todo(text)
+  registered_user.todos.append(new_todo)
+  db.session.add(registered_user)
+  db.session.commit()
+  print("Todo added!")
+
+#flask toggle-todo e.g. flask toggle-todo jane 3
+@click.argument('todo_id', default=1)
+@click.argument('username', default='bob')
+@app.cli.command('toggle-todo')
+def toggle_todo_command(todo_id, username):
+  registered_user = User.query.filter_by(username=username).first()
+  if not registered_user:
+    print(f'{username} not found!')
+    return
+
+  todo = Todo.query.filter_by(id=todo_id, user_id=registered_user.id).first()
+  if not todo:
+    print(f'{username} has no todo id {todo_id}')
+
+  todo.toggle()
+  print(f'{todo.text} is {"completed" if todo.task_completed else "not completed"}!') 
+
+#flask add-category
+@click.argument('username', default='bob')
+@click.argument('todo_id', default=6)
+@click.argument('category', default='chores')
+@app.cli.command('add-category', help="Adds a category to a todo")
+def add_todo_category_command(username, todo_id, category):
+  registered_user = User.query.filter_by(username=username).first()
+  if not registered_user:
+    print(f'{username} not found!')
+    return
+
+  res = registered_user.add_todo_category(todo_id, category)
+  if not res:
+    print(f'{username} has no todo id {todo_id}')
+    return
+
+  print('Category added!')
+
+# I also want to list the categories
+#flask get-categories
+@app.cli.command('get-categories')
+def get_categories():
+  # gets all objects of a model
+  categories = Category.query.all()
+  print(categories)
